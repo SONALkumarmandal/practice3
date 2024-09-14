6996bc966e1237202355669b5ad1954f476ed96d
sonal kumar mandal <152254912+SONALkumarmandal@users.noreply.github.com>
2024-09-14 16:11:55 +0530

    db manage

---
+++ src/models/Video.model.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+const VideoSchema = mongoose.Schema(
+  {
+    videoFil: {
+      type: String,
+      required: true,
+    },
+    thumbnail: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    duration: {
+      type: String,
+      required: true,
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    isPublished: {
+      type: Boolean,
+      default: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+VideoSchema.plugin(mongooseAggregatePaginate)
+export const Video = mongoose.model("Video", VideoSchema);
